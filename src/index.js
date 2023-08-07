2bc2630c4628c6e9c834fdf780573cee2b5ebc23
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-08-07 15:09:19 -0400

    starting code for game.js

--- src/index.js
+++ src/index.js
@@ -7,25 +7,6 @@ class MyGame extends Phaser.Scenef
     {
         super();
     }
-
-    preload ()
-    {
-        this.load.image('logo', logoImg);
-    }
-      
-    create ()
-    {
-        const logo = this.add.image(400, 150, 'logo');
-      
-        this.tweens.add({
-            targets: logo,
-            y: 450,
-            duration: 2000,
-            ease: "Power2",
-            yoyo: true,
-            loop: -1
-        });
-    }
 }
 
 const config = {
