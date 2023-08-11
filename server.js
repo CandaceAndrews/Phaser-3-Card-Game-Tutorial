0398c1511b8dc0aaf7944d799289fbc1a9d37718
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-08-11 19:09:49 -0400

    fixing CORS server error

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const server = require('express')();
 const http = require('http').createServer(server);
 const io = require('socket.io')(http);
+const cors = require('cors');
 
-io.on('connection', function(socket) {
+// Enable CORS for all routes
+server.use(cors());
+
+io.on('connection', function (socket) {
     console.log('A user connected: ' + socket.id);
 
-    socket.on('disconnect', function() {
+    socket.on('disconnect', function () {
         console.log('A user disconnected: ' + socket.id);
     });
 });
 
-http.listen(3000, function() {
+http.listen(3000, function () {
     console.log('Server started!');
 });
