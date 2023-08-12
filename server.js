6c7cdf72ff7314c7ae70b3e47f2d1271492886cd
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-08-12 16:19:17 -0400

    adding more connection logic to server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const server = require('express')();
 const http = require('http').createServer(server);
-// const io = require('socket.io')(http);
+let players = [];
 
 // Enable CORS for all routes
 const io = require("socket.io")(http, {
@@ -10,11 +10,27 @@ const io = require("socket.io")(http, {
     }
 });
 
+
 io.on('connection', function (socket) {
     console.log('A user connected: ' + socket.id);
 
+    players.push(socket.id);
+
+    if (players.length === 1) {
+        io.emit('isPlayerA');
+    };
+
+    socket.on('dealCards', function () {
+        io.emit('dealCards');
+    });
+
+    socket.on('cardPlayed', function (gameObject, isPlayerA) {
+        io.emit('cardPlayed', gameObject, isPlayerA);
+    });
+
     socket.on('disconnect', function () {
         console.log('A user disconnected: ' + socket.id);
+        players = players.filter(player => player !== socket.id);
     });
 });
 
